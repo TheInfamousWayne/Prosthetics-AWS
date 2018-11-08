d5ae955328858385440616d76170dd9077379016
Vaibhav Agrawal <theinfamouswayne@gmail.com>
2018-11-08 21:21:20 +0530

    Saving rewards

--- runner.py
+++ runner.py
@@ -20,8 +20,8 @@ from ddpg import *
 # In[3]:
 
 
-EPISODES = 200
-TEST = 10
+EPISODES = 10000
+TEST = 100
 
 
 # In[4]:
@@ -33,17 +33,23 @@ def main():
 #     env.monitor.start('experiments/' + ENV_NAME,force=True)
     
     # Playing Episodes
+    train_rewards = []
+    avg_rewards = []
     for episode in range(EPISODES):
         state = env.reset()
         #print "episode:",episode
         # Train
+        total_reward = 0
         for step in range(env.spec.timestep_limit):
             action = agent.noise_action(state)
             next_state,reward,done,_ = env.step(action)
             agent.perceive(state,action,reward,next_state,done)
             state = next_state
+            total_reward += reward
             if done:
                 break
+        train_rewards.append(total_reward)
+        
                 
         # Testing:
         if episode % 100 == 0 and episode > 100:
@@ -58,8 +64,12 @@ def main():
                     if done:
                         break
             ave_reward = total_reward/TEST
+            avg_rewards.append(ave_reward)
             print ('episode: ',episode,'Evaluation Average Reward:',ave_reward)
-    
+        
+        if episode % 50 == 0:
+            np.savetxt("train_rewards.csv", train_rewards, delimiter=",", fmt='%d')
+            np.savetxt("avg_rewards.csv", avg_rewards, delimiter=",", fmt='%d')
     # Closing the monitor
 #     env.monitor.close()
 
