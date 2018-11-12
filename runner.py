ad9df9f7fe4f84e9ef0755ed2116ea116f99e463
Ubuntu <ubuntu@ip-172-31-25-206.us-east-2.compute.internal>
2018-11-12 13:34:34 +0000

    printing episode while saving reward and memory

--- runner.py
+++ runner.py
@@ -76,12 +76,12 @@ def main():
             print ('episode: ',episode,'Evaluation Average Reward:',ave_reward)
         
         if episode % 100 == 0:
-            print("Saving Rewards")
+            print("Saving Rewards. Episode: ",episode)
             np.save("train_rewards.npy", train_rewards)
             np.save("average_rewards.npy", avg_rewards)
             
         if episode % 100 == 0:
-            print("Saving Memory")
+            print("Saving Memory. Episode: ",episode)
             agent.replay_buffer.save()
             
     # Closing the monitor
